0674ec5098f7ac438f279c1d5266d895a4ce7a95
holger <hm@fork.de>
2009-01-22 14:41:56 +0100

    =Example configuration

---
+++ install/config.rb
@@ -0,0 +1,14 @@
+module DuckDescribe
+  # example configuration
+  #
+  #def self.config
+  #  {
+  #    'DuckStruct' => {
+  #        :include => :duck_struct_primitives,
+  #        :scope => {},
+  #        :select_name => proc { |p| "#{p.name} - #{p.id}"}
+  #    }
+  #  }
+  end
+end
+
