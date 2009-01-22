20b3d2835d913428ba8511d47d036d6312b01d69
holger <hm@fork.de>
2009-01-22 17:30:18 +0100

    Moved DuckAppearance to models

--- init.rb
+++ init.rb
@@ -2,8 +2,5 @@ require "#{File.dirname(__FILE__)}/lib/duck_describe"
 require "#{Rails.root}/config/duck_config"
 require "#{File.dirname(__FILE__)}/lib/acts_as_duck"
 require "#{File.dirname(__FILE__)}/lib/xml_serializer"
-require "#{File.dirname(__FILE__)}/lib/duck_appearance"
 require 'RMagick'
-
-
 
