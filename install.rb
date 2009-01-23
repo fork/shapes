8f1ebc6a6c395f3e765bf294336f6146db973cdb
holger <hm@fork.de>
2009-01-23 18:31:53 +0100

    =Fixed install.rb

--- install.rb
+++ install.rb
@@ -1,21 +1,14 @@
 require 'fileutils'
 
-module Install
-
-  def copy(basename, target)
-    source = File.join File.dirname __FILE__, 'install', basename
-    target = File.join Rails.root, target
-
-    begin
-      FileUtils.cp source, target, :verbose => true
-    rescue
-      puts "=> failed!"
-    end
-  end
-
-end
-
-puts 'DuckDescribe:'
+puts 'Install config:'
 puts
 
-Install.copy 'config.rb', %w[config duck_config.rb]
+ROOT = File.dirname __FILE__  
+rails_root = File.join ROOT, %w[ .. .. .. ]
+RAILS_ROOT = File.expand_path rails_root
+  
+source = File.join ROOT, 'install', 'config.rb'
+target = File.join RAILS_ROOT, %w[config duck_config.rb]
+
+FileUtils.cp source, target, :verbose => true
+
