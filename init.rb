c59b5fef50123aa583d781162675ab09d4713b4c
Florian Aßmann <flazy@fork.de>
2009-02-06 15:34:03 +0100

    Fixed bug that caused extend_model to fail. Corrected config template.

--- init.rb
+++ init.rb
@@ -1,29 +1,31 @@
-require 'xml/libxml'
 require 'active_record'
-require "#{ File.dirname __FILE__ }/lib/acts/shape/instance_methods"
 require "#{ File.dirname __FILE__ }/lib/shapes"
 require "#{ File.dirname __FILE__ }/lib/shapes/container"
 require "#{ File.dirname __FILE__ }/lib/shapes/resource"
 require "#{ File.dirname __FILE__ }/lib/shapes/primitive"
-require "#{ File.dirname __FILE__ }/lib/xml_serializer"
-
 %w[array boolean datetime enum file float integer string].each do |primitive|
   require "#{ File.dirname __FILE__ }/lib/shapes/primitives/#{ primitive }.rb"
 end
 
+module Shapes::Extension
+  # Allow lazy loading of models while preserving configured models.
+  #
+  # Super must be called before Shapes.extend_model is invoked, otherwise
+  # super overwrites inherited attributes.
+  #
+  # Author: Florian Aßmann (flazy@fork.de) [2009-02-06]
+  def inherited(base)
+    result = super
+    Shapes.extend_model base
+    result
+  end
+end
+ActiveRecord::Base.extend Shapes::Extension
+
 # make rmagick dependency optional
 require 'RMagick' rescue nil
 
-module ActiveRecord #:nodoc:
-  class Base #:nodoc:
-    def self.inherited_with_shapes(base)
-      inherited_without_shapes(base)
-      Shapes.extend_model(base)
-    end
-    class << self
-      alias_method_chain :inherited, :shapes
-    end
-  end
-end
+require 'xml/libxml'
+require "#{ File.dirname __FILE__ }/lib/xml_serializer"
 
 require "#{ Rails.root }/config/shapes_config"
