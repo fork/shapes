a7092d5efce8701f0dcff7f2483878e4c1aa0a2c
holger <hm@fork.de>
2009-02-06 12:22:45 +0100

    Comment config example

--- install/config.rb
+++ install/config.rb
@@ -1,6 +1,6 @@
 module Shapes
-   acts_as_shape 'ShapeStruct',
-     :include      => :shape_struct_primitives,
-     :select_name  => proc { |r| "#{ r.name } - #{ r.id }"},
-     :scope        => proc { |shape| { :conditions => { :language_id => shape.language_id } } }
+   #acts_as_shape 'ShapeStruct',
+   #  :include      => :shape_struct_primitives,
+   #  :select_name  => proc { |r| "#{ r.name } - #{ r.id }"},
+   #  :scope        => proc { |shape| { :conditions => { :language_id => shape.language_id } } }
 end
