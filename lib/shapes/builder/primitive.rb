f7b5ee190624d57288b9c513fa02accc7e868dec
holger <hm@fork.de>
2009-02-09 17:48:34 +0100

    Shift builder strategy classes

--- lib/shapes/builder/primitive.rb
+++ lib/shapes/builder/primitive.rb
@@ -8,15 +8,34 @@ module Shapes
       def initialize(data)
         @data = data
         @builder_strategy = if(data.is_a?(XML::Node))
-          Shapes::Builder::PrimitiveFromXml.new
+          PrimitiveFromXml.new
         else
-          Shapes::Builder::PrimitiveFromHash.new
+          PrimitiveFromHash.new
         end
       end
 
       def build_resource
         @resource = @builder_strategy.build_object @data
       end
+
+      class PrimitiveFromHash
+        def build_object(hash)
+          primitive = "Shapes::Primitives::#{hash[:type]}".constantize.new hash
+          primitive.ident = hash[:ident]
+          primitive.description = hash[:description]
+          primitive
+        end
+      end
+
+      class PrimitiveFromXml
+        def build_object(node)
+          primitive = "Shapes::Primitives::#{node.name.camelize}".constantize.new
+          primitive.xml_node = node
+          primitive.read_from_node
+          primitive
+        end
+      end
+
     end
   end
 end
