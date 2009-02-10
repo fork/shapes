9bd42a2dd10c6d2087093c07abc555718b6ef8b8
alexander <alexander@192.168.10.201>
2009-02-10 17:53:50 +0100

    more rhtml files

---
+++ app/shadows/integer_shadow.rb
@@ -0,0 +1,8 @@
+class IntegerShadow < Shadows::Base
+  def form
+    render_shape(:form)
+  end
+  def form_for_struct
+    render_shape(:form_struct)
+  end
+end
---
+++ app/shadows/struct_shadow.rb
@@ -0,0 +1,5 @@
+class StructShadow < Shadows::Base
+  def form
+    render_shape(:form)
+  end
+end
