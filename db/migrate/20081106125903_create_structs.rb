3d827e3979266db94620d48ff407978ec4c56813
holger <hm@fork.de>
2009-02-10 11:27:52 +0100

    Indexing migrations and preparation for restrictions

--- db/migrate/20081106125903_create_structs.rb
+++ db/migrate/20081106125903_create_structs.rb
@@ -2,9 +2,13 @@ class CreateStructs < ActiveRecord::Migration
   def self.up
     create_table :shape_structs do |t|
       t.string :name, :null => false
-      t.integer :shape_id
+      t.belongs_to :shape
       t.timestamps
     end
+    add_index :shape_structs, :name
+    add_index :shape_structs,
+      [:id, :shape_id],
+      :unique => true, :name => 'struct_shape_index'
   end
 
   def self.down
