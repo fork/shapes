fe1f79e42ecca451ed3edde32c47125ea34ae532
apolzin <alexander@unloved.at>
2009-03-05 18:46:30 +0100

    more helpful error messages, edit link in error messages

--- lib/shapes/constraints/numeric_range.rb
+++ lib/shapes/constraints/numeric_range.rb
@@ -17,13 +17,15 @@ module Shapes
         if (@max && @min) && (@max < @min)
           warning = "Max must be higher than Min."
         elsif (@max && @min) && (@max < resource.value || @min > resource.value)
-          warning = "#{Shapes::NUMERIC_MIN_MAX_WARNING} between #{min} and #{max}."
+          warning = "#{Shapes::NUMERIC_MIN_MAX_WARNING} between #{min} and #{max}"
         elsif (@max && @max < resource.value)
-          warning = "#{Shapes::NUMERIC_MIN_MAX_WARNING} lower than #{max}."
+          warning = "#{Shapes::NUMERIC_MIN_MAX_WARNING} lower than #{max}"
         elsif (@min && @min > resource.value)
-          warning = "#{Shapes::NUMERIC_MIN_MAX_WARNING} higher than #{min}."
+          warning = "#{Shapes::NUMERIC_MIN_MAX_WARNING} higher than #{min}"
         end
-        warning and resource.errors = (resource.errors << Shapes::Error.new({:path => resource.path, :message => warning}))
+        resource.path.slice(/#([a-z]+)/)
+        shape = Shape.find_by_name($1)
+        warning and resource.errors = (resource.errors << Shapes::Error.new({:path => resource.path, :message => warning + " but is #{resource.value} <a href='/shapes/#{shape.id}/edit_resource/#{CGI.escape(resource.path)}'>(edit resource)</a>" }))
       end
     end
   end
