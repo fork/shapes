a21a81173090c0f8008603b5fffbed6bcbb7fb2b
hm <hm@fork.de>
2010-02-03 15:57:04 +0100

    =Comment config example

--- install/config.rb
+++ install/config.rb
@@ -1,8 +1,8 @@
 module Shapes
-  acts_as_shape 'ShapeStruct',
-    :include      => :shape_struct_primitives,
-    :select_name  => proc { "#{ name } - #{ id }" }#,
-    :scope        => proc { |shape|
-      Hash[ :conditions => { :scope_attribute => shape.scope_attribute } ]
-    }
+#  acts_as_shape 'ShapeStruct',
+#    :include      => :shape_struct_primitives,
+#    :select_name  => proc { "#{ name } - #{ id }" },
+#    :scope        => proc { |shape|
+#      Hash[ :conditions => { :scope_attribute => shape.scope_attribute } ]
+#    }
 end
